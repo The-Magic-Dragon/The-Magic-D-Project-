05bd27f9be897f0e19d7534fe4c51096f5313cf3
Audra Dagenais <audluc44@gmail.com>
2020-08-13 22:32:59 -0700

    joke files

---
+++ jokes.js
@@ -0,0 +1,7 @@
+
+{
+    "icon_url" : "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+    "id" : "KKRZPJBXR5iOWY6TzcD4nw",
+    "url" : "",
+    "value" : "Chuck Norris makes big girls cry"
+    }
